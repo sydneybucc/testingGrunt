0dfa165d94905def23dd37098ef12b7c57fe348f
Bucci <s_bucci@tss-hr420-dm18.fc.ca>
2017-12-20 15:26:57 -0500

    first commit

---
+++ js/modules/module1.js
@@ -0,0 +1,7 @@
+var myApp = {
+  mainMessage : "Welcome to the app!",
+
+  mainGreeting() {
+    console.log('hey! welcome to my app!');
+  }
+};
---
+++ js/modules/module3.js
@@ -0,0 +1,5 @@
+myApp.module1 = {
+  doSomethingElse() {
+    console.log('called doSomethingElse from module2');
+  }
+};
