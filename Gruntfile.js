e568eda2c016d5d955169b31e293ea8af56e4b8d
Bucci <s_bucci@tss-hr420-dm18.fc.ca>
2017-12-20 16:24:41 -0500

    third commit

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-sass');
 
-  grunt.registerTask('default', ['concat', 'uglify']);
-  grunt.registerTask('watchFiles', ['watch' 'sass']);
+  grunt.registerTask('default', ['concat', 'uglify', 'sass']);
+  grunt.registerTask('watchFiles', ['watch', 'sass']);
 
 };
